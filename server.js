c1453cd4da8065791a027284bc3daa6cb952a6ac
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-10-09 15:49:43 -0500

    boilerplate

--- server.js
+++ server.js
@@ -27,6 +27,9 @@ app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
+app.post("/notes", (req ,res) => {
+  
+})
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
