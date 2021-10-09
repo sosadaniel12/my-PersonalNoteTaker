e04bcfbd62684b253702a8044b7169b49d18136b
sosadaniel12 <86993622+sosadaniel12@users.noreply.github.com>
2021-10-09 16:47:11 -0500

    made lots of changes

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
 //get all the nodes files I need
+//dependecies
 const express = require("express");
-const route = express.Router();
 const fs = require("fs");
 const path = require("path");
 const notes = require("./db/db.json");
+const shortid = require("shortid");
+const { error } = require("console");
+const { default: ShortUniqueId } = require("short-unique-id");
 
 //create the middleware to have a response
 //Create a post method to send the info to the database
@@ -18,10 +21,35 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
 
-app.post("/api/notes", (req, res) => {});
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get("/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/notes.html"));
+});
+
+app.get("/api/notes", (req, res) => {
+  return res.json(notes);
+});
+
+app.post("/api/notes", (req, res) => {
+  console.log(req.body);
+  let newNote = {
+    id: shortid.generate(),
+    title: req.body.title,
+    text: req.body.text,
+  };
+  console.log(newNote);
+  notes.push(newNote);
+  fs.writeFile("./db/db.json", JSON.stringify(notes), (err) => {
+    if (err) throw err;
+    return res.json(notes);
+  });
+});
+
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
